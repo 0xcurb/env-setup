10b258042155d2d0abf0fe036bbb7f7ca60fb28f
Ahmed Ramadan <ramahmed@gmail.com>
2023-12-28 12:58:27 +0000

    [BugFix] set the custom_host_conf_file variable correctly

--- zsh/conf.d/101-utils.zsh
+++ zsh/conf.d/101-utils.zsh
@@ -8,7 +8,7 @@ conf-key() {
     [[ ! -e $host_conf_file ]] && host_conf_file=''
 
     custom_host_conf_file="$CUSTOM_HOST_CONF_DIR/$(hostname).json"
-    [[ ! -e $custom_host_conf_file ]] && $custom_host_conf_file=''
+    [[ ! -e $custom_host_conf_file ]] && custom_host_conf_file=''
 
     # Search in all files in order and take the first match (first file is highest precedence)
     jq $(printf '.%s' $key) $custom_host_conf_file $host_conf_file $def_conf_file | tr -d '"' | grep -v null | head -1

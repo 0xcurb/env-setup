22829f0349266f94838b145e7a07aaf7696afbcb
Ahmed Ramadan <ramahmed@gmail.com>
2023-12-28 14:27:52 +0000

    [Zsh] Use find to get conf files to source

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -2,10 +2,13 @@ CONF_DIR="$HOME/.config"
 ZSH_CONF_DIR="$CONF_DIR/zsh/conf.d"
 ZSH_CUSTOM_CONF_DIR="$CONF_DIR/custom/zsh"
 
-for c in $(ls $ZSH_CONF_DIR | sort -g); do
-    source $ZSH_CONF_DIR/$c
-done
+source-conf() {
+    source_dir=$1
+    # sort -g does not work with the full paths, using this rudimentary way for now to sort the find output
+    for c in $(find -L $source_dir -type f -name '*.zsh' | awk -F'/' '{printf "%s~-~",$NF; print}' | sort -g | awk -F'~-~' '{print $2}'); do
+        source $c
+    done
+}
 
-for c in $(ls $ZSH_CUSTOM_CONF_DIR | sort -g); do
-    source $ZSH_CUSTOM_CONF_DIR/$c
-done
+source-conf $ZSH_CONF_DIR
+source-conf $ZSH_CUSTOM_CONF_DIR

4727d6394141f6151d469e8e1d45f6920e079fd8
Ahmed Ramadan <ramahmed@gmail.com>
2026-04-26 00:30:42 +0100

    fix (zsh): add linux functions

--- zsh/conf.d/11-linux.zsh
+++ zsh/conf.d/11-linux.zsh
@@ -1,3 +1,21 @@
 is-linux() {
     [[ "$(uname | tr '[:upper:]' '[:lower:]')" =~ "linux" ]]
 }
+
+if is-linux; then
+    _platform-find-pkg-mgr() {
+    }
+    
+    _platform-install-pkg-mgr() {
+    }
+
+    _platform-install-pkg() {
+        sudo yum install $pkg
+    }
+
+    _platform-find-pkg() {
+    }
+
+    _platform-add-pkg-mgr-to-path() {
+    }
+fi

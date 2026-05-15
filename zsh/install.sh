070c7806be12159975ea7282523734a75695c18d
Ahmed Ramadan <ramahmed@gmail.com>
2026-05-15 06:15:44 +0100

    feat (zsh): source local zshrc instead of symlinking

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,6 @@
 self="${BASH_SOURCE[0]}"
 
 symlink "$(self-path $self)/conf.d" "$(config-dir)/$(self-dir-name $self)/conf.d"
-symlink "$(self-path $self)/zshrc.zsh" "$HOME/.zshrc"
+local_zshrc_path="$(self-path $self)/zshrc.zsh"
+source_cmd="source $local_zshrc_path"
+grep "$source_cmd" $HOME/.zshrc 1>/dev/null 2>&1 && echo "$local_zshrc_path is sourced in $HOME/.zshrc" || echo "$source_cmd" >> $HOME/.zshrc
